effe31891eb1345036ed28cb8ed52a4fb954098c
김민석 <alstjr6823@naver.com>
2025-07-01 15:07:09 +0900

    CHORE: 불필요한 코드 제거

--- src/main/java/com/dni_ai_ocr/gateway/config/SwaggerConfig.java
+++ src/main/java/com/dni_ai_ocr/gateway/config/SwaggerConfig.java
@@ -4,8 +4,12 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -24,8 +28,15 @@ public class SwaggerConfig {
                         )
                         .license(new License()
                                 .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")
-                        )
-                );
+                                .url("https://opensource.org/licenses/MIT")));
     }
+
+    @Bean
+    public List<AbstractSwaggerUiConfigProperties.SwaggerUrl> swaggerUrls() {
+        return Arrays.asList(
+                new AbstractSwaggerUiConfigProperties.SwaggerUrl("FastAPI", "/fastapi/openapi.json", "fastapi"),
+                new AbstractSwaggerUiConfigProperties.SwaggerUrl("Spring", "/spring-api-docs/v3/api-docs", "spring")
+        );
+    }
+
 }
